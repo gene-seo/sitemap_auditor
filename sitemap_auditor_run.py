410c2ecc7552235e336d8e7b8fbc8c7ddd9c03cc
gene-seo <117685146+gene-seo@users.noreply.github.com>
2023-02-20 15:20:15 -0500

    Update sitemap_auditor_run.py

--- sitemap_auditor_run.py
+++ sitemap_auditor_run.py
@@ -32,7 +32,8 @@ cached_sess = CacheControl(sess)
 
 response = cached_sess.get('http://google.com')
 
-sitemap_urls = pd.read_excel('C:/Users/Spencer Baselice/OneDrive - Relevate Health/Documents/SEO/SEO Tools/GNE Tools/Sitemaps/sitemap-urls.xlsx"sitemap-urls.xlsx')
+sitemap_urls = pd.read_excel('C:/Users/Spencer Baselice/OneDrive - Relevate Health/Documents/SEO/SEO Tools/GNE Tools/Sitemaps/sitemap-urls.xlsx
+                             ')
 
 sitemap_crawl_list = sitemap_urls.iloc[:, 0].to_list()
 
